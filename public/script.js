c5bbbe1a804ba1e0bd0ec5059c85b5a1305c66e5
Aditi Kumar <aditiswarnakumar@gmail.com>
2021-06-27 12:11:28 +0530

    port

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host:'https://glacial-bayou-14208.herokuapp.com',
+    host:'/',
     port: '3001',
     //secure: 'false'
 })
